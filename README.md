d1572d7cd17fe422c5d9b74ab5d4bd2b406cc5e6
Alex <olexiy.palinkash@gmail.com>
2024-06-13 19:38:03 +0300

    Add sample

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tcpdf-pdf-signing-example
 A self-contained example of basic PDF signing using TCPDF
+
+# PEM generation
+- `http://dss.nowina.lu/pki-factory/keystore/good-user-crl-ocsp.p12`
+- `openssl pkcs12 -info -in good-user-crl-ocsp.p12 -nokeys -legacy > chain.pem`
+- `openssl pkcs12 -info -in good-user-crl-ocsp.p12 -nodes -nocerts --legacy > key.pem`
---
+++ main.php
@@ -0,0 +1,91 @@
+<?php
+//============================================================+
+// Based on https://tcpdf.org/files/examples/example_052.phps
+//============================================================+
+
+// Include the main TCPDF library (search for installation path).
+require_once('vendor/tecnickcom/tcpdf/tcpdf_import.php');
+
+// create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Nicola Asuni');
+$pdf->SetTitle('TCPDF Example 052');
+$pdf->SetSubject('TCPDF Tutorial');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+// set default header data
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 052', PDF_HEADER_STRING);
+
+// set header and footer fonts
+$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// set certificate chain file
+$certificate = 'file://./chain.pem';
+
+// set private key file
+$private_key = 'file://./key.pem';
+
+// set additional information
+$info = array(
+    'Name' => 'TCPDF',
+    'Location' => 'Office',
+    'Reason' => 'Testing TCPDF',
+    'ContactInfo' => 'http://www.tcpdf.org',
+);
+
+// set document signature
+$pdf->setSignature($certificate, $private_key, null, '', 2, $info);
+
+// set font
+$pdf->SetFont('helvetica', '', 12);
+
+// add a page
+$pdf->AddPage();
+
+// print a line of text
+$text = 'This is a <b color="#FF0000">digitally signed document</b> using the default (example) <b>tcpdf.crt</b> certificate.<br />To validate this signature you have to load the <b color="#006600">tcpdf.fdf</b> on the Arobat Reader to add the certificate to <i>List of Trusted Identities</i>.<br /><br />For more information check the source code of this example and the source code documentation for the <i>setSignature()</i> method.<br /><br /><a href="http://www.tcpdf.org">www.tcpdf.org</a>';
+$pdf->writeHTML($text, true, 0, true, 0);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// *** set signature appearance ***
+
+// create content for signature (image and/or text)
+$pdf->Image('images/tcpdf_signature.png', 180, 60, 15, 15, 'PNG');
+
+// define active area for signature appearance
+$pdf->setSignatureAppearance(180, 60, 15, 15);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// *** set an empty signature appearance ***
+$pdf->addEmptySignatureAppearance(180, 80, 15, 15);
+
+// ---------------------------------------------------------
+
+print_r($_SERVER);
+
+//Close and output PDF document
+$pdf->Output('file://' . $_SERVER['PWD'] . '/example_052.pdf', 'F');
+
+
+//============================================================+
+// END OF FILE
+//============================================================+
